5b65296f986c12bf9cc3b482b8441c5c828a2fb1
Jhojan3D <241218432+Jhojan3D@users.noreply.github.com>
2026-02-25 21:51:06 -0500

    Targer

---
+++ Assets/Codigo/SawMovement.cs
@@ -0,0 +1,12 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SawMovement : MonoBehaviour
+{
+    [SerializeField] private Transform saw;
+
+    [SerializeField] private List<Transform> targets = new List<Transform>();
+
+
+}
