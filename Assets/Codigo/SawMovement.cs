3d6d27f030a2ad5ec7ea4615e98e3b119825680c
Jhojan3D <jhojanfabi@gmail.com>
2026-03-04 11:08:08 -0500

    tiempo de recarga y agotamiento de energia

--- Assets/Codigo/SawMovement.cs
+++ Assets/Codigo/SawMovement.cs
@@ -1,4 +1,3 @@
-using NUnit.Framework;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
@@ -10,44 +9,66 @@ public class SawMovement : MonoBehaviour
     [SerializeField] private List<Transform> targets = new List<Transform>();
 
     [SerializeField] private float speed;
+    private float originalSpeed;
 
     [SerializeField] private int currentTarget;
-    private float contador = 2;
-    bool esperando = false;
-    private int direction = 1;  
-
+    private int energia = 5;
+    private float tiempoDeRecarga = 3f;
+    bool recargando = false;
 
     void Start()
+
     {
         saw.position = targets[currentTarget].position;
+        originalSpeed = speed;
     }
     private void Update()
     {
-      
-            if (saw.position == targets[currentTarget].position)
-            {
-                currentTarget += direction;
-                
+        if (recargando == false)
+        { 
+        if (saw.position == targets[currentTarget].position)
+        {
+            currentTarget++;
 
-            if (currentTarget >= targets.Count -1)
+            if (currentTarget >= targets.Count)
+            {
+                currentTarget = 0;
+            }
+
+        }
+
+        saw.position = Vector2.MoveTowards(saw.position, targets[currentTarget].position, speed * Time.deltaTime);
+            if(saw.position == targets[currentTarget].position)
+            {
+                energia--;
+                Debug.Log("Energia: " + energia);
+
+                if(energia <=0)
                 {
-                    direction = -1;
+                    recargando = true;
                 }
-                if (currentTarget <= 0)
+                if (energia <= 1)
                 {
-                    direction = 1;
+                    speed = 0.5f;
+                }
+            }
+
+        }
+        else
+        {
+            tiempoDeRecarga -= Time.deltaTime;
+            if (tiempoDeRecarga <= 0)
+            {
+                recargando = false;
+                tiempoDeRecarga = 3f;
+                energia = 5;
+                speed = originalSpeed;
             }
         }
 
-            saw.position = Vector2.MoveTowards(saw.position, targets[currentTarget].position, speed * Time.deltaTime);
-
-        
-        }
-   
-           
-        
-
     }
+
+}
 
 
 
